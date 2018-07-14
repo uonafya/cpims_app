4549c6d1cf183ca55e613e0a3653341607d9b81d
muneneevans <munene.evansk@gmail.com>
2018-07-14 12:25:32 +0300

    [UPDATE] Button options to Select organizations page

---
+++ src/Screens/SelectOrganization/Button/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    padding: 10,
+    // backgroundColor: "#c32727",
+    borderRadius: 1
+  },
+  buttonText: {
+    color: "#ffffff",
+    textAlign: "center",
+    fontWeight: "700"
+  }
+});
+
+const Button = ({
+  text = "text",
+  backgroundColor = "gray",
+  color = "black"
+}) => {
+  let { buttonContainer, buttonText } = styles;
+  const handleSubmit = () => {
+    //TODO handle submit
+  };
+
+  return (
+    <TouchableOpacity style={[buttonContainer,{backgroundColor,}]} onPress={handleSubmit}>
+      <Text style={[buttonText, {color}]}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
--- src/Screens/SelectOrganization/index.js
+++ src/Screens/SelectOrganization/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
-import {View, Text} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+import Button from "./Button";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    flex: 0.75,
+    maxHeight: 60,
+    minWidth: 250,
+    maxWidth: 350,
+    elevation: 4,
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+});
 
 class SelectOrganization extends Component {
   render() {
+    let { container, buttonContainer } = styles;
     return (
-      <View>
-        <Text >SELECT AN ORGANIZATION</Text>
+      <View style={container}>        
+        <View style={buttonContainer}>
+          <Button text={"USG"} backgroundColor={"#c23616"} color={"#ffffff"} />
+        </View>
+        <View style={buttonContainer}>
+          <Button text={"GOK"} backgroundColor={"#192a56"} color={"#ffffff"} />
+        </View>
       </View>
     );
   }
 }
 
-
-export default SelectOrganization
+export default SelectOrganization;
